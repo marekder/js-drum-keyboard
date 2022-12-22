d6816da0c5aa918026843cb6337b67c371c8daf9
Marek Derlukiewicz <marek.derlukiewicz@gmail.com>
2022-12-22 11:29:46 +0100

    20221222 - script corrected

---
+++ main.js
@@ -0,0 +1,27 @@
+// window.addEventListener('keydown', function (e){
+//   const audio = document.querySelector(`audio[data-key='${e.keyCode}']`);
+//   const key = document.querySelector(`.key[data-key='${e.keyCode}']`);
+//   if(!audio) return; //stop the function running all together
+//   audio.currentTime = 0; // rewind to the start
+//   audio.play();
+//   key.classList.add('playing');
+//
+// });
+
+function playSound(e) {
+    const audio = document.querySelector(`audio[data-key='${e.keyCode}']`);
+    const key = document.querySelector(`.key[data-key='${e.keyCode}']`);
+    if(!audio) return; //stop the function running all together
+    audio.currentTime = 0; // rewind to the start
+    audio.play();
+    key.classList.add('playing');
+};
+
+function removeTransition(e) {
+    if (e.propertyName !== 'transform') return; // skip it if it's not a transform
+    this.classList.remove('playing');
+};
+
+const keys = document.querySelectorAll('.key');
+keys.forEach(key => key.addEventListener('transitionend', removeTransition));
+window.addEventListener('keydown',playSound);
